173d8decf788ef3b3866e73fc0bed13d38932c3f
Quinton Jasper <dropatuningmetal@gmail.com>
2021-02-13 14:07:27 -0700

    Removed use of twine. Opting for poetry publish

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -6,4 +6,4 @@ set -x
 version=$(git describe --tags --abbrev=0)
 poetry version "${version}"
 poetry build $INPUT_POETRY_BUILD_ARGS
-TWINE_USERNAME="$INPUT_PYPI_USERNAME" TWINE_PASSWORD="$INPUT_PYPI_PASSWORD" twine upload $INPUT_TWINE_UPLOAD_ARGS dist/*
+poetry publish --dry-run -u "$INPUT_PYPI_USERNAME" -p "$INPUT_PYPI_PASSWORD" # Remove --dry-run when ready to run actual publishing
